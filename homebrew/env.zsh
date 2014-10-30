aaed6c401d7ea7002bb2b1f0cde4f1028202242b
Ullrich Schäfer <ullrich@seidbereit.de>
2014-10-30 07:46:29 -0700

    Removing unnecessary stderr output from which on some systems

--- homebrew/env.zsh
+++ homebrew/env.zsh
@@ -1,4 +1,4 @@
-if test $(which brew)
+if test $(which brew &>/dev/null)
 then
   export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 fi
--- rbenv/rbenv.zsh
+++ rbenv/rbenv.zsh
@@ -1,4 +1,8 @@
 # rbenv
 
 export RBENV_ROOT=/usr/local/var/rbenv
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+if test $(which rbenv &>/dev/null)
+then
+  eval "$(rbenv init -)"
+fi
