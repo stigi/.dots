d512d3ebafa8b2f3050f7beecb3a71ee0cc82975
Ullrich Schäfer <ullrich@seidbereit.de>
2014-11-17 10:16:22 -0800

    Android SDK

---
+++ android/env.zsh
@@ -0,0 +1,8 @@
+# Android
+if [ -d ~/android-sdk-macosx ]; then
+  export ANDROID_HOME=~/android-sdk-macosx
+fi
+
+if [ -d ~/android-sdk-macosx/tools ]; then
+  export PATH=$PATH:~/android-sdk-macosx/tools
+fi
