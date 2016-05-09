3d7d3bbc71abcec1e03e0afc9fb7e8fbb2706333
Ullrich Schäfer <ullrich+github@seidbereit.de>
2016-05-09 15:34:59 -0700

    Updating aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,4 +1,3 @@
 alias px="pkill -STOP -x Xcode"
 alias cx="pkill -CONT -x Xcode"
-alias buckwilde="px; buck project //Apps/Wilde/Facebook:workspace --without-tests; cx"
-alias buckwildetest="px; buck project wilde; cx"
+alias kx="pkill -KILL -x Xcode"
