54eabb3e7aaf2d9d677b210fee64032e66dc473f
Change your ~/.gitcredentials <hi+this-is-not-ullrich@ullrich.is>
2018-09-06 14:30:39 +0200

    Android prefer emulators in PATH

--- android/env.zsh
+++ android/env.zsh
@@ -3,12 +3,12 @@ if [ -d ${HOME}/Library/Android/sdk ]; then
   export ANDROID_HOME=${HOME}/Library/Android/sdk
 fi
 
-if [ -d ${ANDROID_HOME}/tools ]; then
-  export PATH=${PATH}:${ANDROID_HOME}/tools
-fi
-
 if [ -d ${ANDROID_HOME}/emulator ]; then
   export PATH=${PATH}:${ANDROID_HOME}/emulator
+fi
+
+if [ -d ${ANDROID_HOME}/tools ]; then
+  export PATH=${PATH}:${ANDROID_HOME}/tools
 fi
 
 if [ -d ${ANDROID_HOME}/platform-tools ]; then
