660e1386e59913ec505f95e7e168a01e71134e19
Ullrich Schäfer <git@ullrich.is>
2019-04-18 14:55:14 +0200

    Updating git aliases
    don't mind alias.zsh

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -2,4 +2,4 @@ alias px="pkill -STOP -x Xcode"
 alias cx="pkill -CONT -x Xcode"
 alias kx="pkill -KILL -x Xcode"
 alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias c=code-insiders
+alias c=code
