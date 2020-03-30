f09e0a21b70cea9608390000ab6c90c7353d8a79
Ullrich Schäfer <hi+github@ullrich.is>
2020-03-30 17:15:02 +0200

    Updating NVM

--- nvm/nvm.zsh
+++ nvm/nvm.zsh
@@ -1,8 +1,30 @@
 # NVM
 
-if test ! $(command -v nvm)
+if test ! $(which nvm &>/dev/null)
 then
   export NVM_DIR="$HOME/.nvm"
   [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
+
+  # place this after nvm initialization!
+  autoload -U add-zsh-hook
+  load-nvmrc() {
+    local node_version="$(nvm version)"
+    local nvmrc_path="$(nvm_find_nvmrc)"
+
+    if [ -n "$nvmrc_path" ]; then
+      local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+      if [ "$nvmrc_node_version" = "N/A" ]; then
+        nvm install
+      elif [ "$nvmrc_node_version" != "$node_version" ]; then
+        nvm use
+      fi
+    elif [ "$node_version" != "$(nvm version system)" ]; then
+      echo "Reverting to nvm default system"
+      nvm use system
+    fi
+  }
+  add-zsh-hook chpwd load-nvmrc
+  load-nvmrc
 fi
