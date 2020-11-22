b422090d9f21dd2fe005fa2a7a042a2009f7ec7a
Ullrich Schäfer <hi+gh@ullrich.is>
2020-11-22 20:43:34 +0100

    NVM

--- nvm/nvm.zsh
+++ nvm/nvm.zsh
@@ -20,9 +20,9 @@ then
       elif [ "$nvmrc_node_version" != "$node_version" ]; then
         nvm use
       fi
-    elif [ "$node_version" != "$(nvm version system)" ]; then
-      echo "Reverting to nvm default system"
-      nvm use system
+    elif [ "$node_version" != "$(nvm version default)" ]; then
+      echo "Reverting to nvm default version"
+      nvm use default
     fi
   }
   add-zsh-hook chpwd load-nvmrc
