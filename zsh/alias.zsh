67325d55231e97fa50d150a7d24bbc225d90cc31
Ullrich Schäfer <hi+github@ullrich.is>
2021-10-13 17:46:55 +0200

    Adds some (personal) aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -2,4 +2,5 @@ alias px="pkill -STOP -x Xcode"
 alias cx="pkill -CONT -x Xcode"
 alias kx="pkill -KILL -x Xcode"
 alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias c=code
+alias c=code --enable-proposed-api ms-vscode.vscode-github-issue-notebooks
+alias rn-start="yarn --cwd mobile-app start"
